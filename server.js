da2f9beac56bdca672f65fd8b5b0ce4d7964bc19
Oluwakayode Michael <104535663+oluwakayodemike@users.noreply.github.com>
2023-06-25 19:57:16 +0000

    more to login

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const routes = require("./src/api/routes/routes");
 const tidbConnection = require("./src/db/tiDB");
 const markdownIt = require("markdown-it");
-
+const loginRouter = require('./src/api/user/login');
 const blogController = require("./controller/blogController");
 
 // Setting up the Express server
@@ -25,21 +25,7 @@ app.use((req, res, next) => {
 app.use("/", routes);
 
 // Receiving login details from the frontend
-app.post("/api/user/login", async (req, res) => {
-  try {
-    // extracting user informations from the request payload
-    const { userId, name, email } = req.body;
-
-    // Storing details in TiDB
-    await tidb.storeUserDetails(userId, name, email);
-
-    // Response
-    res.status(200).json({ message: "User details stored successfully" });
-  } catch (error) {
-    console.error("Error storing user details:", error);
-    res.status(500).json({ error: "Failed to store user details" });
-  }
-});
+app.use('/api/user/login', loginRouter);
 
 // get a blog post by id
 app.get("/api/blog/:id", async (req, res) => {
--- src/api/user/login.js
+++ src/api/user/login.js
@@ -5,19 +5,19 @@ const router = express.Router();
 const tidbConnection = require('../db/tiDB');
 
 router.post('/', async (req, res) => {
-    try {
-      // extracting user informations from the request payload
-      const { userId, name, email } = req.body;
-  
-      // Storing details in TiDB
-      await tidbConnection.storeUserDetails(userId, name, email);
-  
-      // Response
-      res.status(200).json({ message: 'User details stored successfully' });
-    } catch (error) {
-      console.error('Error storing user details:', error);
-      res.status(500).json({ error: 'Failed to store user details' });
-    }
-  });
+  try {
+    // Get relevant information from the request payload 
+    const { userid, name, email } = req.body;
+
+    // Store user info in TiDB
+    await tidbConnection.storeUserDetails(userid, name, email);
+
+    // Respond with success status
+    res.status(200).json({ message: 'User details stored successfully' });
+  } catch (error) {
+    console.error('Error storing user details:', error);
+    res.status(500).json({ error: 'Failed to store user details' });
+  }
+});
 
 module.exports = router;
