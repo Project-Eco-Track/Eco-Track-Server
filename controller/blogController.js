e341d9c65ca29d8667b7f781c66600a768e552d6
Akshay-Vs <akshay.vs2005@gmail.com>
2023-07-03 16:02:39 +0530

    Made adjestments for featured blog update

--- controller/blogController.js
+++ controller/blogController.js
@@ -10,16 +10,16 @@ function getAllBlogs(req, res) {
     .fetch(process.env.BLOG_GET_ALL)
     .then((response) => {
       if (!response.ok) {
-        throw new Error("Request failed with status: " + response.status);
+        res.json("Request failed with status: " + response.status);
       }
       return response.json();
     })
     .then((data) => {
-      res.json(data.data.rows); // Send the response as JSON to the client
+      res.json(data.data.rows);
     })
     .catch((error) => {
       console.error("Error:", error);
-      res.status(500).json({ error: "Internal Server Error" }); // Send an error response to the client
+      res.status(500).json({ error: "Internal Server Error" });
     });
 }
 
@@ -28,17 +28,53 @@ function getBlogContent(req, res) {
     .fetch(`${process.env.BLOG_GET_BLOG_POST}?id=${req.params.id}`)
     .then((response) => {
       if (!response.ok) {
-        throw new Error("Request failed with status: " + response.status);
+        res.json("Request failed with status: " + response.status);
       }
       return response.json();
     })
     .then((data) => {
-      res.json(data.data.rows[0]); // Send the response as JSON to the client
+      res.json(data.data.rows[0]);
     })
     .catch((error) => {
       console.error("Error:", error);
-      res.status(500).json({ error: "Internal Server Error" }); // Send an error response to the client
+      res.status(500).json({ error: "Internal Server Error" });
     });
+}
+
+async function getFeaturedBlogID(req, res) { //! not sure if this is needed
+  client
+    .fetch(process.env.BLOG_GET_FEATURED)
+    .then((response) => {
+      if (!response.ok) {
+        res.json("Request failed with status: " + response.status);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      res.json(data.data.rows[0].id);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
+}
+
+// Creating a blog post with user ID
+async function createBlogPost(req, res) {
+  try {
+    const userId = req.userId;
+    const { title, content } = req.body;
+
+    // Insert the blog post into the TiDB database
+    const insertQuery = `INSERT INTO blog_posts (title, content, user_id) VALUES (?, ?, ?)`;
+    const insertValues = [title, content, userId];
+    await req.db.query(insertQuery, insertValues);
+
+    res.status(200).json({ message: "Blog post created successfully" });
+  } catch (error) {
+    console.error("Error creating blog post:", error);
+    res.status(500).json({ error: "Failed to create blog post" });
+  }
 }
 
 module.exports = {
