08047789c5f2024cd696e5295cb638fa39406a06
Oluwakayode Michael <104535663+oluwakayodemike@users.noreply.github.com>
2023-07-17 11:19:01 +0000

    Tested local carbon footprint calculation with weightages and quiz response - Implemented the logic to calculate the carbon footprint based on the researched weightages and quiz response. - Tested the calculation locally without connecting to the server. - Verified that the total carbon footprint is correctly calculated and logged to the terminal. - Rounded the total carbon footprint output to two decimal places for better readability.

--- controller/quizController.js
+++ controller/quizController.js
@@ -1,154 +1,52 @@
-const dietWeightage = require('../src/weightages/dietWeightage').default;
-const energyUsageWeightage = require('../src/weightages/energyUsageWeightage');
-const purchasingHabitWeightage = require('../src/weightages/purchasingHabitWeightage');
-const transportWeightage = require('../src/weightages/transportWeightage');
-const wasteManagementWeightage = require('../src/weightages/wasteManagementWeightage');
+const transportWeightage = require('../src/weightages/transportWeightage.json');
+const dietWeightage = require('../src/weightages/dietWeightage.json');
+const energyUsageWeightage = require('../src/weightages/energyUsageWeightage.json');
+const purchasingHabitWeightage = require('../src/weightages/purchasingHabitWeightage.json');
+const wasteManagementWeightage = require('../src/weightages/wasteManagementWeightage.json');
 
+const quizResponses = require('../quizResponses.json');
 
-// // Calculating Carbon footprint by Quiz Response
-// const calculateCarbonFootprint = (quizResponses) => {
-//   let categoricalCarbonFootprints = [];
-
-//   for (const question in quizResponses) {
-//     const response = quizResponses[question];
-//     let questionWeightage = 0;
-
-//     // Getting weightage by question category
-//     const category = question.split(':')[0];
-
-//     switch (category) {
-//       case 'Diet':
-//         questionWeightage = dietWeightage[response];
-//         break;
-//       case 'Energy Usage':
-//         questionWeightage = energyUsageWeightage[response];
-//         break;
-//       case 'Purchasing Habits':
-//         questionWeightage = purchasingHabitWeightage[response];
-//         break;
-//       case 'Transportation':
-//         questionWeightage = transportWeightage[response];
-//         break;
-//       case 'Waste Management':
-//         questionWeightage = wasteManagementWeightage[response];
-//         break;
-//       default:
-//         // Unrecognized question category handler
-//         break;
-//     }
-
-//     categoricalCarbonFootprints.push(questionWeightage);
-//   }
-
-//   return categoricalCarbonFootprints;
-// };
-
-// const calculateTotalCarbonFootprint = (categoricalCarbonFootprints) => {
-//   let totalCarbonFootprint = 0;
-
-//   // Sum all categorical carbon footprints
-//   for (const categoryCarbonFootprint of categoricalCarbonFootprints) {
-//     totalCarbonFootprint += categoryCarbonFootprint;
-//   }
-
-//   return totalCarbonFootprint;
-// };
-
-// const handleQuiz = (req, res) => {
-//   try {
-//     const quizResponses = req.body;
-//     const categoricalCarbonFootprints = calculateCarbonFootprint(quizResponses);
-//     const totalCarbonFootprint = calculateTotalCarbonFootprint(categoricalCarbonFootprints);
-
-//     // Return carbon footprint as response
-//     res.json({ totalCarbonFootprint });
-//   } catch (error) {
-//     console.error('Error:', error.message);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// };
-
-// module.exports = {
-//   handleQuiz,
-//   calculateCarbonFootprint, // Add the calculateCarbonFootprint function to exports
-// };
-
-const calculateCarbonFootprint = (quizResponses) => {
-  let categoricalCarbonFootprints = [];
-
-  for (const question in quizResponses) {
-    const response = quizResponses[question];
-    let questionWeightage = 0;
-
-    // Getting weightage by question category
-    const category = question.split(':')[0];
-
-    switch (category) {
-      case 'Diet':
-        questionWeightage = dietWeightage[response];
-        console.log('Diet Question Weightage:', questionWeightage);
-        break;
-      case 'Energy Usage':
-        questionWeightage = energyUsageWeightage[response];
-        console.log('Energy Usage Question Weightage:', questionWeightage);
-        break;
-      case 'Purchasing Habits':
-        questionWeightage = purchasingHabitWeightage[response];
-        console.log('Purchasing Habits Question Weightage:', questionWeightage);
-        break;
-      case 'Transportation':
-        questionWeightage = transportWeightage[response];
-        console.log('Transportation Question Weightage:', questionWeightage);
-        break;
-      case 'Waste Management':
-        questionWeightage = wasteManagementWeightage[response];
-        console.log('Waste Management Question Weightage:', questionWeightage);
-        break;
-      default:
-        // Unrecognized question category handler
-        break;
-    }
-
-    categoricalCarbonFootprints.push(questionWeightage);
-
-    // Log the values for debugging
-    console.log('Question:', question);
-    console.log('Response:', response);
-    console.log('Question Weightage:', questionWeightage);
-  }
-
-  return categoricalCarbonFootprints;
+const weightages = {
+  transportWeightage,
+  dietWeightage,
+  energyUsageWeightage,
+  purchasingHabitWeightage,
+  wasteManagementWeightage
 };
 
-const calculateTotalCarbonFootprint = (categoricalCarbonFootprints) => {
+// Calculating the CarbonFootPrint using the Obtained Weightages.
+const calculateCarbonFootprint = (quizResponses, weightages) => {
   let totalCarbonFootprint = 0;
 
-  // Sum all categorical carbon footprints
-  for (const categoryCarbonFootprint of categoricalCarbonFootprints) {
-    totalCarbonFootprint += categoryCarbonFootprint;
-  }
+  for (const category in quizResponses) {
+    const categoryResponses = quizResponses[category];
 
-  // Log the total carbon footprint for debugging
-  console.log('Total Carbon Footprint:', totalCarbonFootprint);
+    for (const question in categoryResponses) {
+      const response = categoryResponses[question];
+
+      // Get the weightage for the response from the weightages object
+      const questionWeightages = weightages[category][question];
+
+      // Checking if the weightage exist for the question
+      if (questionWeightages) {
+        const responseWeightage = questionWeightages[response];
+
+        // Check if the weightage for the response exists
+        if (responseWeightage !== undefined) {
+          // Add the response weightage to the total carbon footprint
+          totalCarbonFootprint += responseWeightage;
+        } else {
+          console.warn(`Weightage not defined for response '${response}' in question '${question}' of category '${category}'. Skipping calculation.`);
+        }
+      } else {
+        console.warn(`Weightage not defined for question '${question}' of category '${category}'. Skipping calculation.`);
+      }
+    }
+  }
 
   return totalCarbonFootprint;
 };
 
-const handleQuiz = (req, res) => {
-  try {
-    const quizResponses = req.body;
-    const categoricalCarbonFootprints = calculateCarbonFootprint(quizResponses);
-    const totalCarbonFootprint = calculateTotalCarbonFootprint(categoricalCarbonFootprints);
-
-    // Return carbon footprint as response
-    res.json({ totalCarbonFootprint });
-  } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
-
-module.exports = {
-  handleQuiz,
-  calculateCarbonFootprint, // Add the calculateCarbonFootprint function to exports
-};
+// Calculate the total carbon footprint
+const totalCarbonFootprint = calculateCarbonFootprint(quizResponses, weightages);
+console.log('Total Carbon Footprint:', totalCarbonFootprint.toFixed(2));
