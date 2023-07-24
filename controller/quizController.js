537c3f2a227e7d4f5ba3d410557a44c9c5797823
oluwakayodemike <michaelaykay@gmail.com>
2023-07-24 19:50:14 +0100

    Refactor quiz controller to handle flat JSON responses and convert to nested format with weightages

--- controller/quizController.js
+++ controller/quizController.js
@@ -1,53 +1,49 @@
 const calculateCarbonFootprint = (req, res) => {
   try {
-    const quizResponses = req.body;
-    console.log('Received Quiz Responses:', quizResponses);
+    const flatQuizResponses = req.body;
+    console.log('Received Quiz Responses:', flatQuizResponses);
 
+    // Loading weightages from the JSON files
     const transportWeightage = require('../src/weightages/transportWeightage.json');
     const dietWeightage = require('../src/weightages/dietWeightage.json');
     const energyUsageWeightage = require('../src/weightages/energyUsageWeightage.json');
     const purchasingHabitWeightage = require('../src/weightages/purchasingHabitWeightage.json');
     const wasteManagementWeightage = require('../src/weightages/wasteManagementWeightage.json');
-  
-    const weightages = {
+
+    // mapping the response to their corresponding categories and questions with weightages and converting it to a nested JSON
+    const mapFlatToNested = (flatResponses, weightages) => {
+      const nestedResponses = {};
+      let totalCarbonFootprint = 0;
+      for (const category in weightages) {
+        nestedResponses[category] = {};
+        const categoryWeightages = weightages[category];
+        for (const question in categoryWeightages) {
+          const weightage = categoryWeightages[question];
+          const response = flatResponses[question];
+          if (response !== undefined) {
+            nestedResponses[category][question] = response;
+            // Apply the weightage to the total carbon footprint
+            totalCarbonFootprint += weightage[response];
+          } else {
+            console.warn(`Response not found for question '${question}' of category '${category}'. Skipping calculation.`);
+          }
+        }
+      }
+      return { nestedResponses, totalCarbonFootprint };
+    };
+
+    // Convert flat JSON object to the desired nested format with weightages
+    const { nestedResponses, totalCarbonFootprint } = mapFlatToNested(flatQuizResponses, {
       transportWeightage,
       dietWeightage,
       energyUsageWeightage,
       purchasingHabitWeightage,
-      wasteManagementWeightage
-    };
-  
-    let totalCarbonFootprint = 0;
+      wasteManagementWeightage,
+    });
 
-    for (const category in quizResponses) {
-      const categoryResponses = quizResponses[category];
-
-      for (const question in categoryResponses) {
-        const response = categoryResponses[question];
-
-        // Get the weightage for the response from the weightages object
-        const questionWeightages = weightages[category][question];
-
-        // Checking if the weightage exists for the question
-        if (questionWeightages) {
-          const responseWeightage = questionWeightages[response];
-
-          // Check if the weightage for the response exists
-          if (responseWeightage !== undefined) {
-            // Add the response weightage to the total carbon footprint
-            totalCarbonFootprint += responseWeightage;
-          } else {
-            console.warn(`Weightage not defined for response '${response}' in question '${question}' of category '${category}'. Skipping calculation.`);
-          }
-        } else {
-          console.warn(`Weightage not defined for question '${question}' of category '${category}'. Skipping calculation.`);
-        }
-      }
-    }
-
+    // Respond with the total carbon footprint
     const roundedCarbonFootprint = totalCarbonFootprint.toFixed(2);
     console.log('Total Carbon Footprint:', roundedCarbonFootprint);
-
     res.json({ totalCarbonFootprint: roundedCarbonFootprint });
   } catch (error) {
     console.error('Error:', error.message);
@@ -58,3 +54,4 @@ const calculateCarbonFootprint = (req, res) => {
 module.exports = {
   calculateCarbonFootprint,
 };
+
